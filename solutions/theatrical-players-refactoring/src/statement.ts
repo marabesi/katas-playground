1fc5ede72402b4da4fc3a7947f072571a080323b
marabesi <matheus.marabesi@gmail.com>
2024-10-27 16:55:22 +0100

    chore(refactoring): inline

--- solutions/theatrical-players-refactoring/src/statement.ts
+++ solutions/theatrical-players-refactoring/src/statement.ts
@@ -68,10 +68,9 @@ function statement(invoice: Invoice, plays: Plays) {
     totalAmount += amountFor(perf);
   }
   
-  let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
   function totalVolumeCredits() {
