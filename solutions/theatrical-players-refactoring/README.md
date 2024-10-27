5bba6329a7574abd07a2839daf9781a1660dba89
marabesi <matheus.marabesi@gmail.com>
2024-10-27 15:29:00 +0100

    chore(refactoring): inline + move function inside statement

---
+++ solutions/theatrical-players-refactoring/README.md
@@ -0,0 +1,17 @@
+# Refactoring
+
+In this file I will keep track of the refactorings used to reach the final solution for the kata. I am using vscode
+and webstorm and comparing the autoated refactorings that each one of them offers.
+
+> Note: vscode used out-of-the-box, no extensions used
+
+| Refactoring | Vscode | Webstorm |
+|-------------|--------|----------|
+| Extract funtion - select the block of code you want to extract in a method | ✅  Cmd + .  | ✅ cmd + option + m |
+| Replace temp with query | ❌ (Manual) | ❌ (Manual) |
+| Inline - position the cursor in the variable that you want to inline | ✅  Cmd + .  |  cmd + optoin + n |
+| | | |
+| | | |
+| | | |
+| | | |
+| | | |
--- solutions/theatrical-players-refactoring/src/statement.ts
+++ solutions/theatrical-players-refactoring/src/statement.ts
@@ -17,27 +17,6 @@ type Invoice = {
 
 type Play = { name: string; type: string };
 
-function amountFor(play: Play, aPerformance: Performance) {
-  let thisAmount = 0;
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (aPerformance.audience > 30) {
-        thisAmount += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (aPerformance.audience > 20) {
-        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      thisAmount += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-  return thisAmount;
-}
 
 function statement(invoice: Invoice, plays: Plays) {
   let totalAmount = 0;
@@ -48,20 +27,41 @@ function statement(invoice: Invoice, plays: Plays) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
+  
+  function amountFor(play: Play, aPerformance: Performance) {
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (aPerformance.audience > 30) {
+          thisAmount += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (aPerformance.audience > 20) {
+          thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        thisAmount += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
+  }
 
   function playFor(aPerformance: Performance) {
     return plays[aPerformance.playID];
   }
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(play, perf);
+    let thisAmount = amountFor(playFor(perf), perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === (playFor(perf)).type) volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${(playFor(perf)).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
