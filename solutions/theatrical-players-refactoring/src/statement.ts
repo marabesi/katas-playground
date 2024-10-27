b08c18126b58a887bb7776934c1c58a54f6d75d9
marabesi <matheus.marabesi@gmail.com>
2024-10-27 16:52:02 +0100

    chore(refactoring): split loop

--- solutions/theatrical-players-refactoring/src/statement.ts
+++ solutions/theatrical-players-refactoring/src/statement.ts
@@ -65,10 +65,14 @@ function statement(invoice: Invoice, plays: Plays) {
   }
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
     result += ` ${(playFor(perf)).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+  
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
