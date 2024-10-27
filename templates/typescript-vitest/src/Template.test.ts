d83c139cd325e00fecfa9587f76fb776711b8969
marabesi <matheus.marabesi@gmail.com>
2024-10-27 21:32:04 +0100

    chore(templates): created typescript-vitest

---
+++ templates/typescript-vitest/src/Template.test.ts
@@ -0,0 +1,9 @@
+import { expect, describe, it } from 'vitest'
+
+import { Template } from './Template'
+
+describe('Template', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  })
+})
