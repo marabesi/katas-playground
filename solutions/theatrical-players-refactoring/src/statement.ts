8b9986f03098a210bb7663282b5ced427948d220
marabesi <matheus.marabesi@gmail.com>
2024-10-27 17:02:39 +0100

    chore(refactoring): rename

--- solutions/theatrical-players-refactoring/src/statement.ts
+++ solutions/theatrical-players-refactoring/src/statement.ts
@@ -71,19 +71,19 @@ function statement(invoice: Invoice, plays: Plays) {
   return result;
 
   function totalAmount() {
-    let totalAmount = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf);
+      result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
 }
 
